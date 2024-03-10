517911ffd7a15471bd5771a0736cd53485106c03
yashdesai <yash.desai.naik@gmail.com>
2024-03-10 23:00:35 +0530

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,101 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.btnStart = New System.Windows.Forms.Button()
+        Me.btnStop = New System.Windows.Forms.Button()
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
+        Me.ToolStripStatusLabel1 = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.StatusStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'btnStart
+        '
+        Me.btnStart.BackColor = System.Drawing.Color.DodgerBlue
+        Me.btnStart.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnStart.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnStart.Font = New System.Drawing.Font("Calibri", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnStart.ForeColor = System.Drawing.Color.White
+        Me.btnStart.Location = New System.Drawing.Point(57, 56)
+        Me.btnStart.Name = "btnStart"
+        Me.btnStart.Size = New System.Drawing.Size(125, 51)
+        Me.btnStart.TabIndex = 0
+        Me.btnStart.Text = "Start"
+        Me.btnStart.UseVisualStyleBackColor = False
+        '
+        'btnStop
+        '
+        Me.btnStop.BackColor = System.Drawing.Color.Crimson
+        Me.btnStop.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnStop.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnStop.Font = New System.Drawing.Font("Calibri", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnStop.ForeColor = System.Drawing.Color.White
+        Me.btnStop.Location = New System.Drawing.Point(188, 56)
+        Me.btnStop.Name = "btnStop"
+        Me.btnStop.Size = New System.Drawing.Size(125, 51)
+        Me.btnStop.TabIndex = 0
+        Me.btnStop.Text = "Stop"
+        Me.btnStop.UseVisualStyleBackColor = False
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.ImageScalingSize = New System.Drawing.Size(24, 24)
+        Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripStatusLabel1})
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 146)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.Size = New System.Drawing.Size(380, 32)
+        Me.StatusStrip1.TabIndex = 1
+        Me.StatusStrip1.Text = "StatusStrip1"
+        '
+        'ToolStripStatusLabel1
+        '
+        Me.ToolStripStatusLabel1.Name = "ToolStripStatusLabel1"
+        Me.ToolStripStatusLabel1.Size = New System.Drawing.Size(180, 25)
+        Me.ToolStripStatusLabel1.Text = "ToolStripStatusLabel1"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(380, 178)
+        Me.Controls.Add(Me.StatusStrip1)
+        Me.Controls.Add(Me.btnStop)
+        Me.Controls.Add(Me.btnStart)
+        Me.ForeColor = System.Drawing.Color.White
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide
+        Me.Text = "Scotia Task Scheduler"
+        Me.TopMost = True
+        Me.StatusStrip1.ResumeLayout(False)
+        Me.StatusStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnStart As Button
+    Friend WithEvents btnStop As Button
+    Friend WithEvents StatusStrip1 As StatusStrip
+    Friend WithEvents ToolStripStatusLabel1 As ToolStripStatusLabel
+End Class
---
+++ Form1.vb
@@ -0,0 +1,217 @@
+﻿Imports System.IO
+
+Public Class Form1
+
+    Private Sub MainForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        CheckTaskStatus()
+    End Sub
+
+    Private Sub CheckTaskStatus()
+        Dim taskExists As Boolean = TaskIsScheduled("scotia-email-drafts")
+
+        If taskExists Then
+            btnStart.Enabled = False
+            btnStop.Enabled = True
+
+            ' Get the next run time of the task and update the status bar
+            Dim nextRunTime As String = GetNextRunTime("scotia-email-drafts")
+            ToolStripStatusLabel1.Text = "Next run time: " & nextRunTime
+            ToolStripStatusLabel1.ForeColor = Color.Black
+            If nextRunTime IsNot Nothing Then
+
+            End If
+        Else
+            btnStart.Enabled = True
+            btnStop.Enabled = False
+            ToolStripStatusLabel1.Text = "Task not scheduled"
+            ToolStripStatusLabel1.ForeColor = Color.Black
+        End If
+    End Sub
+
+    Private Sub btnStart_Click(sender As Object, e As EventArgs) Handles btnStart.Click
+        Dim openFileDialog As New OpenFileDialog()
+        openFileDialog.Filter = "Executable Files (*.exe)|*.exe"
+        openFileDialog.Title = "Select Executable File"
+
+        If openFileDialog.ShowDialog() = DialogResult.OK Then
+            Dim executablePath As String = openFileDialog.FileName
+            CreateTask(executablePath)
+        End If
+    End Sub
+    Private Sub CreateTask(executablePath As String)
+        Try
+            ' Define the XML content for the task definition
+            Dim xmlContent As String = $"<?xml version='1.0' encoding='UTF-16'?>
+<Task version='1.2' xmlns='http://schemas.microsoft.com/windows/2004/02/mit/task'>
+  <RegistrationInfo>
+    <Date>2024-03-10T20:38:52</Date>
+    <Author>YASH-PC\yash</Author>
+    <URI>\scotia-email-drafts</URI>
+  </RegistrationInfo>
+  <Triggers>
+    <CalendarTrigger>
+      <StartBoundary>2024-03-10T00:00:00</StartBoundary>
+      <Enabled>true</Enabled>
+      <ScheduleByMonth>
+        <DaysOfMonth>
+          <Day>1</Day>
+        </DaysOfMonth>
+        <Months>
+          <January />
+          <February />
+          <March />
+          <April />
+          <May />
+          <June />
+          <July />
+          <August />
+          <September />
+          <October />
+          <November />
+          <December />
+        </Months>
+      </ScheduleByMonth>
+    </CalendarTrigger>
+  </Triggers>
+  <Principals>
+    <Principal id='Author'>
+      <UserId>S-1-5-21-2800104608-1952058749-1654487197-1002</UserId>
+      <LogonType>InteractiveToken</LogonType>
+      <RunLevel>LeastPrivilege</RunLevel>
+    </Principal>
+  </Principals>
+  <Settings>
+    <MultipleInstancesPolicy>IgnoreNew</MultipleInstancesPolicy>
+    <DisallowStartIfOnBatteries>true</DisallowStartIfOnBatteries>
+    <StopIfGoingOnBatteries>true</StopIfGoingOnBatteries>
+    <AllowHardTerminate>true</AllowHardTerminate>
+    <StartWhenAvailable>true</StartWhenAvailable>
+    <RunOnlyIfNetworkAvailable>false</RunOnlyIfNetworkAvailable>
+    <IdleSettings>
+      <StopOnIdleEnd>true</StopOnIdleEnd>
+      <RestartOnIdle>false</RestartOnIdle>
+    </IdleSettings>
+    <AllowStartOnDemand>true</AllowStartOnDemand>
+    <Enabled>true</Enabled>
+    <Hidden>false</Hidden>
+    <RunOnlyIfIdle>false</RunOnlyIfIdle>
+    <WakeToRun>false</WakeToRun>
+    <ExecutionTimeLimit>PT1H</ExecutionTimeLimit>
+    <Priority>7</Priority>
+    <RestartOnFailure>
+      <Interval>PT1M</Interval>
+      <Count>3</Count>
+    </RestartOnFailure>
+  </Settings>
+  <Actions Context='Author'>
+    <Exec>
+      <Command>{executablePath}</Command>
+    </Exec>
+  </Actions>
+</Task>
+        "
+
+            ' Save the XML content to a temporary file
+            Dim tempXmlFile As String = Path.Combine(Path.GetTempPath(), "TaskDefinition.xml")
+            File.WriteAllText(tempXmlFile, xmlContent)
+
+            ' Create the task using schtasks /create /xml
+            Dim processInfo As New ProcessStartInfo("schtasks", $"/Create /XML ""{tempXmlFile}"" /TN ""scotia-email-drafts""")
+            processInfo.CreateNoWindow = True
+            processInfo.UseShellExecute = False
+            processInfo.RedirectStandardError = True
+
+            Using process As Process = Process.Start(processInfo)
+                process.WaitForExit()
+                Dim errorMessage As String = process.StandardError.ReadToEnd()
+                If Not String.IsNullOrEmpty(errorMessage) Then
+                    MessageBox.Show("Error creating task: " & errorMessage)
+                Else
+                    MessageBox.Show("Task created successfully!")
+                    CheckTaskStatus()
+                End If
+            End Using
+
+            ' Delete the temporary XML file
+            File.Delete(tempXmlFile)
+        Catch ex As Exception
+            MessageBox.Show("Error creating task: " & ex.Message)
+        End Try
+    End Sub
+
+
+    Private Sub btnStop_Click(sender As Object, e As EventArgs) Handles btnStop.Click
+        Try
+            ' Delete the task named "scotia-email-drafts"
+            Dim processInfo As New ProcessStartInfo("schtasks", "/Delete /TN ""scotia-email-drafts"" /F")
+            processInfo.CreateNoWindow = True
+            processInfo.UseShellExecute = False
+            processInfo.RedirectStandardError = True
+
+            Using process As Process = Process.Start(processInfo)
+                process.WaitForExit()
+                Dim errorMessage As String = process.StandardError.ReadToEnd()
+                If Not String.IsNullOrEmpty(errorMessage) Then
+                    MessageBox.Show("Error stopping task: " & errorMessage)
+                Else
+                    MessageBox.Show("Task stopped successfully!")
+                    CheckTaskStatus()
+                End If
+            End Using
+        Catch ex As Exception
+            MessageBox.Show("Error stopping task: " & ex.Message)
+        End Try
+    End Sub
+
+    Private Function TaskIsScheduled(taskName As String) As Boolean
+        Dim processInfo As New ProcessStartInfo("schtasks", "/Query /TN """ & taskName & """")
+        processInfo.CreateNoWindow = True
+        processInfo.UseShellExecute = False
+        processInfo.RedirectStandardOutput = True
+
+        Using process As Process = Process.Start(processInfo)
+            process.WaitForExit()
+
+            ' Read the standard output to check if the task exists
+            Dim output As String = process.StandardOutput.ReadToEnd()
+            Return output.ToLower().Contains(taskName.ToLower())
+        End Using
+    End Function
+    Private Function GetNextRunTime(taskName As String) As String
+        Dim processInfo As New ProcessStartInfo("schtasks", "/Query /TN """ & taskName & """")
+        processInfo.CreateNoWindow = True
+        processInfo.UseShellExecute = False
+        processInfo.RedirectStandardOutput = True
+
+        Dim nextRunTime As String = "Not available"
+
+        Using process As Process = Process.Start(processInfo)
+            process.WaitForExit()
+
+            ' Read the standard output to find the next run time
+            Dim output As String = process.StandardOutput.ReadToEnd()
+
+            ' Split the output into lines
+            Dim lines() As String = output.Split({Environment.NewLine}, StringSplitOptions.RemoveEmptyEntries)
+
+
+            Console.WriteLine(lines.Length)
+
+            ' Check if there are enough lines
+            If lines.Length >= 4 Then
+                ' Get the third line and split it by whitespace
+                Dim parts() As String = lines(3).Split({" "}, StringSplitOptions.RemoveEmptyEntries)
+                Console.WriteLine(parts.Length)
+                ' Check if there are enough parts
+                If parts.Length >= 3 Then
+                    ' Join the remaining parts after removing the first two words and the last word
+                    nextRunTime = String.Join(" ", parts.Skip(1).Take(parts.Length - 3))
+                    Console.WriteLine(nextRunTime)
+                End If
+            End If
+        End Using
+
+        Return nextRunTime
+    End Function
+
+End Class
